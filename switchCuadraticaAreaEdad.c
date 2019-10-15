86ce742449f4261aabfd45d71af05d1e449bf2ae
LuisCoralesMartinez <55764759+LuisCoralesMartinez@users.noreply.github.com>
2019-10-15 15:34:26 -0500

    Add files via upload

---
+++ switchCuadraticaAreaEdad.c
@@ -0,0 +1,81 @@
+/******************************************************************************
+Realizar un programa que contenga el menu con las siguientes opciones
+*******************************************************************************/
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    int op, dia, mes, year, diaActual, mesActual, yearActual, diaCal, mesCal, yearCal;
+    float a, b, c, x1 = 0, x2 = 0, base, altura, area = 0;
+    
+    do
+    {
+        
+        printf("\nMenu principal\nEscoja\n1. Ecuación cuadrática\n2. Área del triángulo\n3. Edad\n");
+        scanf("%i", &op);
+        
+        switch(op)
+        {
+            case 1:
+                printf("Ingrese el coeficiente a: ");
+                scanf("%f", &a);
+                printf("Ingrese el coeficiente b: ");
+                scanf("%f", &b);
+                printf("Ingrese el coeficiente c: ");
+                scanf("%f", &c);
+                
+                x1 = (-b + (sqrt(pow(b,2) - 4 * a * c))) / (2 * a);
+                x2 = (-b - (sqrt(pow(b,2) - 4 * a * c))) / (2 * a);
+                
+                printf("X1 = %f\n", x1);
+                printf("X2 = %f\n", x2);
+            break;
+    
+            case 2:
+                printf("Ingrese la base: ");
+                scanf("%f", &base);
+                printf("Ingrese la altura: ");
+                scanf("%f", &altura);
+    
+                area = (base * altura) / 2;
+    
+                printf("Area = %.2f", area);
+            break;
+            
+            case 3:
+                printf("Ingrese el dia: ");
+                scanf("%i", &dia);
+                printf("Ingrese el mes: ");
+                scanf("%i", &mes);
+                printf("Ingrese el año: ");
+                scanf("%i", &year);
+                
+                diaActual = 15;
+                mesActual = 10;
+                yearActual = 2019;
+                
+                if(dia > diaActual){
+                    dia = diaActual - dia;
+                    mesActual = mesActual - 1;
+                }
+                
+                if(mes > mesActual){
+                    yearActual = yearActual - 1;
+                    mesActual = mesActual + 12;
+                }
+                
+                diaCal = diaActual - dia;
+                mesCal = mesActual - mes;
+                yearCal = yearActual - year;
+                
+                printf("Su edad es %i años, %i meses y %i dias\n", yearCal, mesCal, diaCal);
+                
+            break;
+            
+            default:
+                printf("Opción no válida\n");
+            break;
+        }
+    }while(op > 0 && op < 4);
+}
